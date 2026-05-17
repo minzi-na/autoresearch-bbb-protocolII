1cd25fd3fe28aa59ab00ff1e11f93376247c40e4
minzi-na <minzikim@snu.ac.kr>
2026-05-18 07:33:00 +0900

    iter51: remove pre-head LayerNorm

--- train.py
+++ train.py
@@ -189,7 +189,7 @@ class MultiModalGMLPFromFlat(nn.Module):
             Xp = pooled.mean(dim=1)
         else:
             Xp = X.mean(dim=1)
-        Xp = self.drop(self.norm(Xp))
+        Xp = self.drop(Xp)
         return self.head(Xp).squeeze(-1)
 
 
