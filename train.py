b13ab1aab820799b34d33c040dff19579b3dd2aa
minzi-na <minzikim@snu.ac.kr>
2026-05-18 03:51:47 +0900

    Revert "iter27: skip-gate mix between attention-pool and max-pool"
    This reverts commit 513bda5a4a5e6196c2f25be8957bc0bb2cda290c.

--- train.py
+++ train.py
@@ -188,9 +188,9 @@ class MultiModalGMLPFromFlat(nn.Module):
             w = torch.softmax(scores, dim=1)
             pooled = torch.einsum("bsk,bsd->bkd", w, X)
             gated = pooled.mean(dim=1)
-            mx = X.max(dim=1).values
+            mean = X.mean(dim=1)
             g = torch.sigmoid(self.skip_gate)
-            Xp = g * gated + (1.0 - g) * mx
+            Xp = g * gated + (1.0 - g) * mean
         else:
             Xp = X.mean(dim=1)
         Xp = self.drop(self.norm(Xp))
