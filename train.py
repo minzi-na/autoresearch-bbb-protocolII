7d245be6892f34fcc2495d6fd3949630e6874f4f
minzi-na <minzikim@snu.ac.kr>
2026-05-18 05:49:29 +0900

    iter39: label smoothing=0.05 + EMA=0.9 + grad-clip

--- train.py
+++ train.py
@@ -226,7 +226,8 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
         for x, y in train_loader:
             x, y = x.to(device), y.to(device)
             optimizer.zero_grad()
-            loss = loss_fn(model(x), y)
+            y_smooth = y * (1.0 - 0.05) + 0.05 * 0.5
+            loss = loss_fn(model(x), y_smooth)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
