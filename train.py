64ce9e351d1ffb7260b50685fdefe8eb1aa35d13
minzi-na <minzikim@snu.ac.kr>
2026-05-18 06:09:14 +0900

    Revert "iter54: modality dropout p=0.08 (fine sweep)"
    This reverts commit 8ad58b2278bacc72074129f0181ebf2b0f6af6e3.

--- train.py
+++ train.py
@@ -141,7 +141,7 @@ class MultiModalGMLPFromFlat(nn.Module):
             self.skip_gate = nn.Parameter(torch.zeros(1))
         self.head = nn.Linear(d_model, 1)
         self.drop = nn.Dropout(dropout)
-        self.mod_drop_p = 0.08
+        self.mod_drop_p = 0.075
 
     def forward(self, x):
         chunks = torch.split(x, self.mod_dims, dim=1)
