17cd28273a7aa82021a026dc30700f3dd94b56b0
minzi-na <minzikim@snu.ac.kr>
2026-05-18 04:04:31 +0900

    iter39: LayerScale (init 1e-4) on gMLPBlock output

--- train.py
+++ train.py
@@ -90,13 +90,14 @@ class SpatialGatingUnit(nn.Module):
 
 
 class gMLPBlock(nn.Module):
-    def __init__(self, d_model, d_ffn, seq_len, drop_path=0.025):
+    def __init__(self, d_model, d_ffn, seq_len, drop_path=0.025, layer_scale_init=1e-4):
         super().__init__()
         self.norm = nn.LayerNorm(d_model)
         self.channel_proj1 = nn.Linear(d_model, d_ffn * 2)
         self.channel_proj2 = nn.Linear(d_ffn, d_model)
         self.sgu = SpatialGatingUnit(d_ffn, seq_len)
         self.drop_path = drop_path
+        self.layer_scale = nn.Parameter(layer_scale_init * torch.ones(d_model))
 
     def forward(self, x):
         residual = x
@@ -106,7 +107,7 @@ class gMLPBlock(nn.Module):
         x = F.gelu(self.channel_proj1(x))
         x = self.sgu(x)
         x = self.channel_proj2(x)
-        return x + residual
+        return self.layer_scale * x + residual
 
 
 class gMLP(nn.Module):
