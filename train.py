acd276ab3f40ffd88d50833489ae6436cac34715
minzi-na <minzikim@snu.ac.kr>
2026-05-18 03:15:05 +0900

    Revert "iter32: 2-layer head with GELU"
    This reverts commit 35f6bc933fc6dca2bd9340ff57fa450e936f0a7a.

--- train.py
+++ train.py
@@ -139,11 +139,7 @@ class MultiModalGMLPFromFlat(nn.Module):
         if use_gated_pool:
             self.alpha = nn.Parameter(torch.zeros(self.seq_len))
             self.skip_gate = nn.Parameter(torch.zeros(1))
-        self.head = nn.Sequential(
-            nn.Linear(d_model, d_model // 2),
-            nn.GELU(),
-            nn.Linear(d_model // 2, 1),
-        )
+        self.head = nn.Linear(d_model, 1)
         self.drop = nn.Dropout(dropout)
         self.mod_drop_p = 0.05
 
