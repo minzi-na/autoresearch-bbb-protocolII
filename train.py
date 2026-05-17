46db5f112d947dd26bb39a534d2e4ebf044acd85
minzi-na <minzikim@snu.ac.kr>
2026-05-18 05:46:57 +0900

    Revert "iter51: AdamW betas=(0.9, 0.95)"
    This reverts commit 4aa775322959cfaa735258e98e52563908c7f6af.

--- train.py
+++ train.py
@@ -361,7 +361,6 @@ def build_and_train(
             {"params": no_decay_params, "weight_decay": 0.0},
         ],
         lr=BASE_CONFIG["lr"],
-        betas=(0.9, 0.95),
     )
     loss_fn = nn.BCEWithLogitsLoss()
 
