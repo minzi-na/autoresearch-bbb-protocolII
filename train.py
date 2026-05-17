3fe5343d3d7940e987fb0c482d920d649ac28354
minzi-na <minzikim@snu.ac.kr>
2026-05-18 03:45:30 +0900

    Revert "iter26: gradient clipping max_norm=1.0 only (isolated from iter2 combo)"
    This reverts commit 7e731478c4d7e4947fe818c3c1fae00471879955.

--- train.py
+++ train.py
@@ -225,7 +225,6 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
             optimizer.zero_grad()
             loss = loss_fn(model(x), y)
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
             tr_loss_sum += loss.item()
             tr_batches  += 1
