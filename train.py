778d5a5c3dd28204f997a6ec1af074473295a62b
minzi-na <minzikim@snu.ac.kr>
2026-05-18 04:36:02 +0900

    iter43: alpha init biased toward mole modality

--- train.py
+++ train.py
@@ -137,7 +137,8 @@ class MultiModalGMLPFromFlat(nn.Module):
                              d_ffn=d_ffn, num_layers=depth)
         self.norm = nn.LayerNorm(d_model)
         if use_gated_pool:
-            self.alpha = nn.Parameter(torch.zeros(self.seq_len))
+            alpha_init = torch.tensor([1.0 if n == "mole" else -0.5 for n in self.mod_names])
+            self.alpha = nn.Parameter(alpha_init)
             self.skip_gate = nn.Parameter(torch.zeros(1))
         self.head = nn.Linear(d_model, 1)
         self.drop = nn.Dropout(dropout)
