05987f1f850765b5c0315201eb329f27e566032c
minzi-na <minzikim@snu.ac.kr>
2026-05-18 06:21:52 +0900

    Revert "iter42: head dropout=0.1 (reduce from 0.2; EMA already regularizes)"
    This reverts commit c3d1220f74d568da08378861ba5d5c1a4bb69720.

--- train.py
+++ train.py
@@ -174,7 +174,7 @@ class MultiModalGMLPFromFlat(nn.Module):
             self.pool_queries = nn.Parameter(torch.zeros(2, d_model))
             self.skip_gate = nn.Parameter(torch.zeros(1))
         self.head = nn.Linear(d_model, 1)
-        self.drop = nn.Dropout(0.1)
+        self.drop = nn.Dropout(dropout)
 
     def forward(self, x):
         chunks = torch.split(x, self.mod_dims, dim=1)
