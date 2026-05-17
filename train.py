0007c2bd4210aa61159d98106d386907cd64a16a
minzi-na <minzikim@snu.ac.kr>
2026-05-18 06:45:53 +0900

    iter59: pool_logits init favor max [0, 0, 1]

--- train.py
+++ train.py
@@ -138,7 +138,7 @@ class MultiModalGMLPFromFlat(nn.Module):
         self.norm = nn.LayerNorm(d_model)
         if use_gated_pool:
             self.alpha = nn.Parameter(torch.zeros(self.seq_len))
-            self.pool_logits = nn.Parameter(torch.zeros(3))
+            self.pool_logits = nn.Parameter(torch.tensor([0.0, 0.0, 1.0]))
         self.head = nn.Linear(d_model, 1)
         self.drop = nn.Dropout(dropout)
         self.mod_drop_p = 0.075
