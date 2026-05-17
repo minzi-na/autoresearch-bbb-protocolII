c4640ced30bfa0dfdb5a5aa84ea6732ac8f950d5
minzi-na <minzikim@snu.ac.kr>
2026-05-18 04:21:49 +0900

    iter41: inverted modality dropout (rescale by 1/(1-p))

--- train.py
+++ train.py
@@ -151,6 +151,7 @@ class MultiModalGMLPFromFlat(nn.Module):
         if self.training and self.mod_drop_p > 0:
             B = X.shape[0]
             mask = (torch.rand(B, self.seq_len, device=X.device) > self.mod_drop_p).float()
+            mask = mask / (1 - self.mod_drop_p)
             X = X * mask.unsqueeze(-1)
         X = self.backbone(X)
         if self.use_gated_pool:
