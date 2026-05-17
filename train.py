8617e3f2f36976d2a4c2d9a404f085ff4f17c5b9
minzi-na <minzikim@snu.ac.kr>
2026-05-18 03:18:52 +0900

    iter23: multi-query attention pool k=4 (matches seq_len)

--- train.py
+++ train.py
@@ -171,7 +171,7 @@ class MultiModalGMLPFromFlat(nn.Module):
                              d_ffn=d_ffn, num_layers=depth)
         self.norm = nn.LayerNorm(d_model)
         if use_gated_pool:
-            self.pool_queries = nn.Parameter(torch.zeros(2, d_model))
+            self.pool_queries = nn.Parameter(torch.zeros(4, d_model))
             self.skip_gate = nn.Parameter(torch.zeros(1))
         self.head = nn.Linear(d_model, 1)
         self.drop = nn.Dropout(dropout)
