9a09e218182a3cfa2435f44b38d579a1cc8b8657
minzi-na <minzikim@snu.ac.kr>
2026-05-18 02:45:39 +0900

    Revert "iter28: label smoothing 0.05 (smaller than iter4's 0.1)"
    This reverts commit 01d8f3ccdfb2319f99c3526d8030eccf5dc33cc9.

--- train.py
+++ train.py
@@ -194,9 +194,7 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
         for x, y in train_loader:
             x, y = x.to(device), y.to(device)
             optimizer.zero_grad()
-            smooth = 0.05
-            y_smooth = y * (1 - smooth) + smooth * 0.5
-            loss = loss_fn(model(x), y_smooth)
+            loss = loss_fn(model(x), y)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
