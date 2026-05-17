de012f8e5ba8261fde1d61df6bd5f08aeeb9711f
minzi-na <minzikim@snu.ac.kr>
2026-05-18 02:26:48 +0900

    iter17: EMA of weights (decay=0.999) for early-stop snapshot

--- train.py
+++ train.py
@@ -216,6 +216,9 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
     bad = 0
     epoch_log = []
 
+    ema_decay = 0.999
+    ema_state = {k: v.detach().clone() for k, v in model.state_dict().items()}
+
     for epoch in range(num_epochs):
         model.train()
         tr_loss_sum, tr_batches = 0.0, 0
@@ -228,6 +231,16 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
             tr_loss_sum += loss.item()
             tr_batches  += 1
         train_loss = tr_loss_sum / max(tr_batches, 1)
+
+        with torch.no_grad():
+            for k, v in model.state_dict().items():
+                if v.dtype.is_floating_point:
+                    ema_state[k].mul_(ema_decay).add_(v.detach(), alpha=1.0 - ema_decay)
+                else:
+                    ema_state[k].copy_(v.detach())
+
+        saved_state = {k: v.detach().clone() for k, v in model.state_dict().items()}
+        model.load_state_dict(ema_state)
 
         model.eval()
         val_loss_sum, val_batches = 0.0, 0
@@ -258,13 +271,15 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
         score = val_auc if es_metric == "val_auc" else val_loss
         if is_better(score, best_score):
             best_score = score
-            best_state = deepcopy(model.state_dict())
+            best_state = {k: v.detach().clone() for k, v in ema_state.items()}
             best_epoch = epoch
             bad = 0
         else:
             bad += 1
-            if bad >= patience:
-                break
+
+        model.load_state_dict(saved_state)
+        if bad >= patience:
+            break
 
     if best_state is not None:
         model.load_state_dict(best_state)
