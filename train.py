7e83cf7d7d2d4eabf1536b98b1f70610b644e275
minzi-na <minzikim@snu.ac.kr>
2026-05-17 19:43:54 +0900

    Revert "iter2: AdamW + warmup(5)/cosine + grad-clip(1.0)"
    This reverts commit 9f6a4482d294c61185d4a03181fded45050abf48.

--- train.py
+++ train.py
@@ -15,7 +15,6 @@ Do NOT edit:
   - device handling
 """
 
-import math
 import random
 from copy import deepcopy
 from collections import OrderedDict
@@ -172,14 +171,6 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
     bad = 0
     epoch_log = []
 
-    warmup_epochs = 5
-    def lr_lambda(epoch):
-        if epoch < warmup_epochs:
-            return (epoch + 1) / warmup_epochs
-        p = (epoch - warmup_epochs) / max(1, num_epochs - warmup_epochs)
-        return 0.5 * (1 + math.cos(math.pi * p)) * 0.99 + 0.01
-    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
-
     for epoch in range(num_epochs):
         model.train()
         tr_loss_sum, tr_batches = 0.0, 0
@@ -188,11 +179,9 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
             optimizer.zero_grad()
             loss = loss_fn(model(x), y)
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
             tr_loss_sum += loss.item()
             tr_batches  += 1
-        scheduler.step()
         train_loss = tr_loss_sum / max(tr_batches, 1)
 
         model.eval()
@@ -331,7 +320,7 @@ def build_and_train(
         use_gated_pool=BASE_CONFIG["use_gated_pool"],
     ).to(device)
 
-    optimizer = optim.AdamW(
+    optimizer = optim.Adam(
         model.parameters(),
         lr=BASE_CONFIG["lr"],
         weight_decay=BASE_CONFIG["weight_decay"],
