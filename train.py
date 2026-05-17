55bfe1adb315e4118104cb12ff221e0ad3363a9e
minzi-na <minzikim@snu.ac.kr>
2026-05-18 08:30:48 +0900

    iter72: pool_query random init (std=0.1) instead of zero

--- train.py
+++ train.py
@@ -138,7 +138,7 @@ class MultiModalGMLPFromFlat(nn.Module):
         self.norm = nn.LayerNorm(d_model)
         if use_gated_pool:
             self.alpha = nn.Parameter(torch.zeros(self.seq_len))
-            self.pool_query = nn.Parameter(torch.zeros(d_model))
+            self.pool_query = nn.Parameter(torch.randn(d_model) * 0.1)
             self.pool_logits = nn.Parameter(torch.zeros(4))
         self.head = nn.Linear(d_model, 1)
         self.drop = nn.Dropout(dropout)
