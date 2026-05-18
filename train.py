b06cb21ad28de4cdfb086b94ddf70ef0bf3cec40
minzi-na <minzikim@snu.ac.kr>
2026-05-18 10:01:01 +0900

    Revert "iter83: EMA decay 0.9985 on quad-pool stack"
    This reverts commit 97738ea065dc7de053e17022c7e6dd3b4548df62.

--- train.py
+++ train.py
@@ -190,7 +190,7 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
     bad = 0
     epoch_log = []
 
-    ema_decay = 0.9985
+    ema_decay = 0.999
     ema_state = {k: v.detach().clone() for k, v in model.state_dict().items()}
 
     for epoch in range(num_epochs):
