932a78bb3d87f9acd432606cb079871d78ec813f
minzi-na <minzikim@snu.ac.kr>
2026-05-18 16:58:04 +0900

    Revert "iter79: CLS + mean pool average (50/50 mix)"
    This reverts commit 1c924aba912fc667ad84e05ecd4f54885a210ac7.

--- train.py
+++ train.py
@@ -189,7 +189,7 @@ class MultiModalGMLPFromFlat(nn.Module):
         cls = self.cls_token.expand(X.size(0), -1, -1)
         X = torch.cat([cls, X], dim=1)
         X = self.backbone(X)
-        Xp = 0.5 * (X[:, 0, :] + X[:, 1:, :].mean(dim=1))
+        Xp = X[:, 0, :]
         Xp = self.drop(self.norm(Xp))
         return self.head(Xp).squeeze(-1)
 
