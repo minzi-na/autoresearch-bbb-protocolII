a2f6f1516dc94ecdc56e67bdde9dae78a40f8d5c
minzi-na <minzikim@snu.ac.kr>
2026-05-18 17:17:27 +0900

    iter81: R-Drop alpha 1.0 -> 0.5 (sweep down)

--- train.py
+++ train.py
@@ -248,7 +248,8 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
                      + (1 - p1) * ((1 - p1).log() - (1 - p2).log())).mean()
             kl_21 = (p2 * (p2.log() - p1.log())
                      + (1 - p2) * ((1 - p2).log() - (1 - p1).log())).mean()
-            rdrop_alpha = 1.0
+            # iter81: sweep R-Drop alpha 1.0 -> 0.5 (less consistency pressure).
+            rdrop_alpha = 0.5
             loss = bce_loss + rdrop_alpha * 0.5 * (kl_12 + kl_21)
             loss.backward()
             optimizer.step()
