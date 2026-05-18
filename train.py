24818469cb9b161de45f82d7634b10349299ab41
minzi-na <minzikim@snu.ac.kr>
2026-05-18 15:28:53 +0900

    iter92: sigmoid pool_logits (unbounded sum) init logit(0.25)

--- train.py
+++ train.py
@@ -140,7 +140,7 @@ class MultiModalGMLPFromFlat(nn.Module):
         if use_gated_pool:
             self.alpha = nn.Parameter(torch.zeros(self.seq_len))
             self.pool_query = nn.Parameter(torch.zeros(d_model))
-            self.pool_logits = nn.Parameter(torch.zeros(4))
+            self.pool_logits = nn.Parameter(torch.full((4,), -1.0986))
         self.head = nn.Linear(d_model, 1)
         self.drop = nn.Dropout(dropout)
         self.mod_drop_p = 0.075
@@ -164,7 +164,7 @@ class MultiModalGMLPFromFlat(nn.Module):
             attn_scores = (X @ self.pool_query) / (X.shape[-1] ** 0.5)
             attn_w = torch.softmax(attn_scores, dim=1)
             attn_pool = (X * attn_w.unsqueeze(-1)).sum(dim=1)
-            pw = torch.softmax(self.pool_logits, dim=0)
+            pw = torch.sigmoid(self.pool_logits)
             Xp = pw[0] * gated + pw[1] * mean + pw[2] * max_pool + pw[3] * attn_pool
         else:
             Xp = X.mean(dim=1)
