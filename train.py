280b7f76725f1686ca7deafe6f4e61b9bcc21f09
minzi-na <minzikim@snu.ac.kr>
2026-05-19 09:27:34 +0900

    Revert "iter4: label smoothing 0.1 on train targets"
    This reverts commit b6b3593695720181bba465cc019513ed19fb56b8.

--- train.py
+++ train.py
@@ -177,8 +177,7 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
         for x, y in train_loader:
             x, y = x.to(device), y.to(device)
             optimizer.zero_grad()
-            y_smooth = y * 0.9 + 0.05
-            loss = loss_fn(model(x), y_smooth)
+            loss = loss_fn(model(x), y)
             loss.backward()
             optimizer.step()
             tr_loss_sum += loss.item()
