a7f7caee068d1bfa18b6c8227e8784f7d53e7c5e
minzi-na <minzikim@snu.ac.kr>
2026-05-19 11:03:28 +0900

    iter23: AdamW only (no warmup/cosine; with grad-clip already KEPT)

--- train.py
+++ train.py
@@ -341,7 +341,7 @@ def build_and_train(
         use_gated_pool=BASE_CONFIG["use_gated_pool"],
     ).to(device)
 
-    optimizer = optim.Adam(
+    optimizer = optim.AdamW(
         model.parameters(),
         lr=BASE_CONFIG["lr"],
         weight_decay=BASE_CONFIG["weight_decay"],
