4b493d484db7fd7e06761df1b5ae823bdfe46df1
minzi-na <minzikim@snu.ac.kr>
2026-05-20 04:15:23 +0900

    Revert "iter148: mod_drop_p 0.125"
    This reverts commit 84533615f7a7035dba39165b05addc5cf8ecb1e6.

--- train.py
+++ train.py
@@ -156,7 +156,7 @@ class MultiModalGMLPFromFlat(nn.Module):
             nn.Linear(d_model * 2, 1),
         )
         self.drop = nn.Dropout(dropout)
-        self.mod_drop_p = 0.125
+        self.mod_drop_p = 0.1
 
     def forward(self, x):
         chunks = torch.split(x, self.mod_dims, dim=1)
