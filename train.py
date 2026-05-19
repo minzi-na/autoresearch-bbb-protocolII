7c152cae9a3d3b2dc84229a4c5aa610262ce4d4e
minzi-na <minzikim@snu.ac.kr>
2026-05-20 08:38:04 +0900

    iter166: AdamW + wd=1e-6 override (replaces param_group wd override)

--- train.py
+++ train.py
@@ -216,8 +216,11 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
 
     patience = 20
 
-    for param_group in optimizer.param_groups:
-        param_group['weight_decay'] = 1e-6
+    optimizer = optim.AdamW(
+        model.parameters(),
+        lr=BASE_CONFIG["lr"],
+        weight_decay=1e-6,
+    )
 
     ema_decay = 0.82
     ema_state = None
