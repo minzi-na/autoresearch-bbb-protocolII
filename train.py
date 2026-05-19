cb9252b3a24b1669f81408bc539cdadf563b1a65
minzi-na <minzikim@snu.ac.kr>
2026-05-20 02:00:52 +0900

    Revert "iter130: dropout on full backbone output (before CLS select), norm after"
    This reverts commit 9d6e51d42cc0a0539e0401875415654de027c257.

--- train.py
+++ train.py
@@ -189,8 +189,8 @@ class MultiModalGMLPFromFlat(nn.Module):
         cls = self.cls_token.expand(X.size(0), -1, -1)
         X = torch.cat([cls, X], dim=1)
         X = self.backbone(X)
-        X = self.drop(X)
-        Xp = self.norm(X[:, 0, :])
+        Xp = X[:, 0, :]
+        Xp = self.drop(self.norm(Xp))
         return self.head(Xp).squeeze(-1)
 
 
