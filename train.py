7e4ff072119fbdec3a13f3c9aff810778d9fe749
minzi-na <minzikim@snu.ac.kr>
2026-05-20 05:42:11 +0900

    iter151: weight_decay=1e-6 + iter150 stack

--- train.py
+++ train.py
@@ -217,7 +217,7 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
     patience = 15
 
     for param_group in optimizer.param_groups:
-        param_group['weight_decay'] = 5e-6
+        param_group['weight_decay'] = 1e-6
 
     ema_decay = 0.815
     ema_state = None
