5bdb6ff83f26928bf5528a95c0e9bb5a0691bdee
minzi-na <minzikim@snu.ac.kr>
2026-05-19 22:55:02 +0900

    iter125: SGU Conv1d bias init=0.5 (between 0 and 1)

--- train.py
+++ train.py
@@ -83,7 +83,7 @@ class SpatialGatingUnit(nn.Module):
             nn.Conv1d(seq_len, seq_len, kernel_size=1) for _ in range(n_heads)
         ])
         for proj in self.spatial_projs:
-            nn.init.constant_(proj.bias, 1.0)
+            nn.init.constant_(proj.bias, 0.5)
         self.gate_scale = nn.Parameter(torch.zeros(1))
 
     def forward(self, x):
