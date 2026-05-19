1da4f777e751c97ecd7a2775e347e21a4324dfee
minzi-na <minzikim@snu.ac.kr>
2026-05-19 10:15:42 +0900

    Revert "iter13: AdaLN variant of cross-modal FiLM (category-based)"
    This reverts commit fad03f0ef94f3473f1350de715ec3d76570e59de.

--- train.py
+++ train.py
@@ -139,31 +139,11 @@ class MultiModalGMLPFromFlat(nn.Module):
         self.head = nn.Linear(d_model, 1)
         self.drop = nn.Dropout(dropout)
 
-        _fp_set = {"maccs", "avalon"}
-        _emb_set = {"scage1", "scage2", "mole"}
-        self.fp_idx = [i for i, n in enumerate(self.mod_names) if n in _fp_set]
-        self.emb_idx = [i for i, n in enumerate(self.mod_names) if n in _emb_set]
-        self.adaln_norm = nn.LayerNorm(d_model, elementwise_affine=False)
-        self.adaln_e2f = nn.Linear(d_model, d_model * 2)
-        self.adaln_f2e = nn.Linear(d_model, d_model * 2)
-        nn.init.zeros_(self.adaln_e2f.weight); nn.init.zeros_(self.adaln_e2f.bias)
-        nn.init.zeros_(self.adaln_f2e.weight); nn.init.zeros_(self.adaln_f2e.bias)
-
     def forward(self, x):
         chunks = torch.split(x, self.mod_dims, dim=1)
         tokens = [self.proj[name](chunk)
                   for name, chunk in zip(self.mod_names, chunks)]
         X = torch.stack(tokens, dim=1)
-        if len(self.fp_idx) > 0 and len(self.emb_idx) > 0:
-            fp_sum  = X[:, self.fp_idx,  :].mean(dim=1)
-            emb_sum = X[:, self.emb_idx, :].mean(dim=1)
-            gf, bf = self.adaln_e2f(emb_sum).chunk(2, dim=-1)
-            ge, be = self.adaln_f2e(fp_sum).chunk(2, dim=-1)
-            X_norm = self.adaln_norm(X)
-            X_film = X.clone()
-            X_film[:, self.fp_idx,  :] = (1 + gf.unsqueeze(1)) * X_norm[:, self.fp_idx,  :] + bf.unsqueeze(1)
-            X_film[:, self.emb_idx, :] = (1 + ge.unsqueeze(1)) * X_norm[:, self.emb_idx, :] + be.unsqueeze(1)
-            X = X_film
         X = self.backbone(X)
         if self.use_gated_pool:
             w = torch.softmax(self.alpha, dim=0)
