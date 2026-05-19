756a6d910805ed3133749ce14bde1edb1874dba6
minzi-na <minzikim@snu.ac.kr>
2026-05-20 05:25:32 +0900

    Revert "iter149: lr=1.1e-4 override + iter141 stack"
    This reverts commit 6b93627b046ca4b7d8047e9ec8536154e7f63044.

--- train.py
+++ train.py
@@ -216,9 +216,6 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
 
     patience = 15
 
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = 1.1e-4
-
     ema_decay = 0.815
     ema_state = None
 
