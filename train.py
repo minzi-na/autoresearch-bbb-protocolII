fee4069aafcf425d81c6dd683ab4a833150eb55e
minzi-na <minzikim@snu.ac.kr>
2026-05-19 09:43:27 +0900

    Revert "iter8: CrossModalFiLM (fp=[maccs], emb=[scage1,mole])"
    This reverts commit eb9920168d32b4bcc803d3ecdf2e7a10f519ae24.

--- train.py
+++ train.py
@@ -115,27 +115,6 @@ class gMLP(nn.Module):
         return self.model(x)
 
 
-class CrossModalFiLM(nn.Module):
-    def __init__(self, d_model, fp_idx, emb_idx):
-        super().__init__()
-        self.embed_to_fp = nn.Linear(d_model, d_model * 2)
-        self.fp_to_embed = nn.Linear(d_model, d_model * 2)
-        nn.init.zeros_(self.embed_to_fp.weight); nn.init.zeros_(self.embed_to_fp.bias)
-        nn.init.zeros_(self.fp_to_embed.weight); nn.init.zeros_(self.fp_to_embed.bias)
-        self.fp_idx = fp_idx
-        self.emb_idx = emb_idx
-
-    def forward(self, X):
-        fp_sum  = X[:, self.fp_idx,  :].mean(1)
-        emb_sum = X[:, self.emb_idx, :].mean(1)
-        gf, bf = self.embed_to_fp(emb_sum).chunk(2, dim=-1)
-        ge, be = self.fp_to_embed(fp_sum).chunk(2, dim=-1)
-        X = X.clone()
-        X[:, self.fp_idx,  :] = (1 + gf.unsqueeze(1)) * X[:, self.fp_idx,  :] + bf.unsqueeze(1)
-        X[:, self.emb_idx, :] = (1 + ge.unsqueeze(1)) * X[:, self.emb_idx, :] + be.unsqueeze(1)
-        return X
-
-
 class MultiModalGMLPFromFlat(nn.Module):
     def __init__(self, mod_dims: OrderedDict, d_model=512, d_ffn=1024,
                  depth=4, dropout=0.2, use_gated_pool=True):
@@ -149,10 +128,6 @@ class MultiModalGMLPFromFlat(nn.Module):
             name: nn.Linear(in_dim, d_model)
             for name, in_dim in zip(self.mod_names, self.mod_dims)
         })
-        fp_set = {"maccs", "avalon"}
-        fp_idx = [i for i, n in enumerate(self.mod_names) if n in fp_set]
-        emb_idx = [i for i, n in enumerate(self.mod_names) if n not in fp_set]
-        self.film = CrossModalFiLM(d_model, fp_idx, emb_idx)
         self.backbone = gMLP(seq_len=self.seq_len, d_model=d_model,
                              d_ffn=d_ffn, num_layers=depth)
         self.norm = nn.LayerNorm(d_model)
@@ -167,7 +142,6 @@ class MultiModalGMLPFromFlat(nn.Module):
         tokens = [self.proj[name](chunk)
                   for name, chunk in zip(self.mod_names, chunks)]
         X = torch.stack(tokens, dim=1)
-        X = self.film(X)
         X = self.backbone(X)
         if self.use_gated_pool:
             w = torch.softmax(self.alpha, dim=0)
