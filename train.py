7c724130d60a8cb22fa83190cf6f1d7e35f5bed8
minzi-na <minzikim@snu.ac.kr>
2026-05-19 18:59:10 +0900

    iter101: EMA init from epoch-0 trained weights (skip init bias)

--- train.py
+++ train.py
@@ -217,7 +217,7 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
     patience = 15
 
     ema_decay = 0.83
-    ema_state = {k: v.detach().clone() for k, v in model.state_dict().items()}
+    ema_state = None
 
     for epoch in range(num_epochs):
         model.train()
@@ -234,11 +234,14 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
         train_loss = tr_loss_sum / max(tr_batches, 1)
 
         with torch.no_grad():
-            for k, v in model.state_dict().items():
-                if v.dtype.is_floating_point:
-                    ema_state[k].mul_(ema_decay).add_(v.detach(), alpha=1.0 - ema_decay)
-                else:
-                    ema_state[k].copy_(v.detach())
+            if ema_state is None:
+                ema_state = {k: v.detach().clone() for k, v in model.state_dict().items()}
+            else:
+                for k, v in model.state_dict().items():
+                    if v.dtype.is_floating_point:
+                        ema_state[k].mul_(ema_decay).add_(v.detach(), alpha=1.0 - ema_decay)
+                    else:
+                        ema_state[k].copy_(v.detach())
 
         saved_state = {k: v.detach().clone() for k, v in model.state_dict().items()}
         model.load_state_dict(ema_state)
