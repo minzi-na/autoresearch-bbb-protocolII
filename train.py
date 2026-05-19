3c0f3ec635d636f9bb24028939f9b511ea28890b
minzi-na <minzikim@snu.ac.kr>
2026-05-19 11:40:43 +0900

    Revert "iter25: modality dropout p=0.10 (between 0.05 kept and 0.15 failed)"
    This reverts commit 2763c173250527d55da0d24a3de895586de61117.

--- train.py
+++ train.py
@@ -148,7 +148,7 @@ class MultiModalGMLPFromFlat(nn.Module):
             self.skip_gate = nn.Parameter(torch.zeros(1))
         self.head = nn.Linear(d_model, 1)
         self.drop = nn.Dropout(dropout)
-        self.mod_drop_p = 0.10
+        self.mod_drop_p = 0.05
 
     def forward(self, x):
         chunks = torch.split(x, self.mod_dims, dim=1)
