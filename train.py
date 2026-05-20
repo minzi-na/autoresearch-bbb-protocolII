e57b849b757fd6d55c4c8635775d33426f1854de
minzi-na <minzikim@snu.ac.kr>
2026-05-20 12:26:59 +0900

    Revert "iter174: stop-gradient on FiLM gating signal (sender detach)"
    This reverts commit ad3a4621e2bbeef8ffba6174b058d6711b688bea.

--- train.py
+++ train.py
@@ -141,8 +141,8 @@ class CrossModalFiLM(nn.Module):
         emb_tokens = X[:, self.emb_idx, :]
         fp_sum  = fp_tokens.mean(1)
         emb_sum = emb_tokens.mean(1)
-        gf, bf = self.embed_to_fp(emb_sum.detach()).chunk(2, dim=-1)
-        ge, be = self.fp_to_embed(fp_sum.detach()).chunk(2, dim=-1)
+        gf, bf = self.embed_to_fp(emb_sum).chunk(2, dim=-1)
+        ge, be = self.fp_to_embed(fp_sum).chunk(2, dim=-1)
         fp_norm  = self.ln(fp_tokens)
         emb_norm = self.ln(emb_tokens)
         Xn = X.clone()
