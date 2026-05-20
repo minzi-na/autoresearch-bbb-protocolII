daa9a6711123daeb732ce61537b8ba2899cca47b
minzi-na <minzikim@snu.ac.kr>
2026-05-20 15:57:17 +0900

    iter190: proj EMA decay=0.7 + head=0.2 + rest=0.82

--- train.py
+++ train.py
@@ -241,7 +241,12 @@ def train_model(model, optimizer, train_loader, val_loader, loss_fn,
                 ema_state = {k: v.detach().clone() for k, v in model.state_dict().items()}
             else:
                 for k, v in model.state_dict().items():
-                    decay = 0.2 if k.startswith('head.') else ema_decay
+                    if k.startswith('head.'):
+                        decay = 0.2
+                    elif k.startswith('proj.'):
+                        decay = 0.7
+                    else:
+                        decay = ema_decay
                     if v.dtype.is_floating_point:
                         ema_state[k].mul_(decay).add_(v.detach(), alpha=1.0 - decay)
                     else:
